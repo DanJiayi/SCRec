69d803d4632b1c839af88bb15b34d2c5c4546387
DanJiayi <danjiayi123@163.com>
2026-03-08 22:52:57 +0800

    1

--- run.sh
+++ run.sh
@@ -7,8 +7,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --n_codebook=32 \
     --contrastive_alpha=0.5 \
     --manifold_c=0.5 \
-    --manifold_beta=0.2 \
-    --epochs=300
+    --epochs=500
 
 
 CUDA_VISIBLE_DEVICES=0 python main.py \
@@ -19,8 +18,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --n_codebook=16 \
     --contrastive_alpha=0.5 \
     --manifold_c=0.2 \
-    --manifold_beta=0.2 \
-    --epochs=300
+    --epochs=500
 
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --model=CSA \
@@ -37,40 +35,3 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
 
 
 
-
-
-
-
-CUDA_VISIBLE_DEVICES=0 python main.py \
-    --model=CSA \
-    --category=Beauty \
-    --lr=0.01 \
-    --temperature=0.03 \
-    --n_codebook=32 \
-    --contrastive_alpha=0.5 \
-    --manifold_c=0.5 \
-    --manifold_beta=0 \
-    --epochs=300
-
-
-CUDA_VISIBLE_DEVICES=0 python main.py \
-    --model=CSA \
-    --category=Sports_and_Outdoors \
-    --lr=0.003 \
-    --temperature=0.03 \
-    --n_codebook=16 \
-    --contrastive_alpha=0.5 \
-    --manifold_c=0.2 \
-    --manifold_beta=0 \
-    --epochs=300
-
-CUDA_VISIBLE_DEVICES=0 python main.py \
-    --model=CSA \
-    --category=Toys_and_Games \
-    --lr=0.01 \
-    --temperature=0.03 \
-    --n_codebook=16 \
-    --contrastive_alpha=0.5 \
-    --manifold_c=0.2 \
-    --manifold_beta=0 \
-    --epochs=300
