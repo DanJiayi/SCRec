948b33a5f3318165bd40a40ee1c0534d35aa4879
DanJiayi <danjiayi123@163.com>
2026-03-14 04:58:11 +0800

    up

--- run.sh
+++ run.sh
@@ -6,19 +6,19 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --temperature=0.03 \
     --n_codebook=32 \
     --contrastive_alpha=0.5 \
-    --manifold_c=0.5 \
-    --epochs=500
-
+    --manifold_beta=0.2 \
+    --manifold_c=0.5
 
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --model=CSA \
     --category=Sports_and_Outdoors \
-    --lr=0.003 \
+    --lr=0.01 \
     --temperature=0.03 \
     --n_codebook=16 \
     --contrastive_alpha=0.5 \
-    --manifold_c=0.2 \
-    --epochs=500
+    --manifold_beta=0.2 \
+    --manifold_c=1 \
+    --code_weight_tau=2.0
 
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --model=CSA \
@@ -27,11 +27,6 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --temperature=0.03 \
     --n_codebook=16 \
     --contrastive_alpha=0.5 \
-    --manifold_c=0.2 \
     --manifold_beta=1 \
-    --epochs=300
-
-
-
-
-
+    --manifold_c=1 \
+    --code_weight_tau=2.0
